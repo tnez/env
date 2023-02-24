724782b3d33cd06389d61d64856abb2dbedd67ac
Travis Nesland <tnesland@gmail.com>
2023-02-24 20:56:41 +0000

    feat: allow setting of a default value to return if value is not present in process.env

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,12 @@ dotenv.config();
 
 const env = process.env;
 
-export function getEnv(key: string): string {
-  const value = env[key];
+export type GetEnvOptions = Partial<{
+  default: string;
+}>;
+
+export function getEnv(key: string, options: GetEnvOptions = {}): string {
+  const value = env[key] ?? options.default;
   if (!value) {
     throw new Error(`Environment variable ${key} is not defined`);
   }
